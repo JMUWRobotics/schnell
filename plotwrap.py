7204536cbaa09dc4165da0fcfe61241803aca202
Christoph Liebender <christoph.liebender@uni-wuerzburg.de>
2024-12-17 11:38:01 +0100

    add water plane plot to plotwrap

--- plotwrap.py
+++ plotwrap.py
@@ -2,15 +2,18 @@
 
 import subprocess, json
 import numpy as np
+import os
 from matplotlib import pyplot as plt
+
+datadir = os.environ["HOME"] + '/Daten'
 
 schnell = subprocess.Popen(
     [
         'builddir/schnell',
-        '../wasserkiste/no-waves/0_left.png',
-        '../wasserkiste/no-waves/0_right.png',
-        '../Kalibrierungen/DISCO3D/Luft/left.yaml',
-        '../Kalibrierungen/DISCO3D/Luft/right.yaml',
+        datadir + '/wasserkiste/no-waves-pattern/0_left.png',
+        datadir + '/wasserkiste/no-waves-pattern/0_right.png',
+        datadir + '/Kalibrierungen/DISCO3D/Luft/left.yaml',
+        datadir + '/Kalibrierungen/DISCO3D/Luft/right.yaml',
         'april'
     ],
     stdout = subprocess.PIPE
@@ -31,10 +34,24 @@ plane_pt   = np.array(j["someplane"  ]["pt"])
 plane_abcd = np.array(j["someplane"  ]["abcd"])
 scenepts   = np.array(j["scenepoints"])
 
-print(baseline)
+def meshgrid(pt, abcd, xlim, ylim):
+    (xlo, xhi), (ylo, yhi) = xlim, ylim
+    xx, yy = np.mgrid[0:101, 0:101] / 100
+
+    xx = xx * (xhi - xlo) / (xx.max() - xx.min()) + xlo
+    yy = yy * (yhi - ylo) / (yy.max() - yy.min()) + ylo
+        
+    a, b, c, _ = abcd
+    x0, y0, z0 = pt
+        
+    z = a*(xx - x0) + b*(yy - y0) / -c + z0
+            
+    return xx, yy, z
 
 fig = plt.figure()
 ax  = fig.add_subplot(projection='3d')
+
+ax.plot_surface(*meshgrid(plane_pt, plane_abcd, (-0.2, 0.5), (-0.2, 0.2)), alpha = 0.2, label='water surface')
 
 ax.scatter(scenepts[:, 0], scenepts[:, 1], scenepts[:, 2], marker='.', color='green', label='correspondence')
 ax.scatter(0, 0, 0, marker='o', color='black', label='left cam')
